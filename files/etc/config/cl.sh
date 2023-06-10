93e97ac0370615f6adcd40f466ddda3c695d05ce
sun3et <72607204+sun3et@users.noreply.github.com>
2023-06-10 16:00:53 +0800

    Delete cl.sh

--- files/etc/config/cl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd /etc/
-wget --no-check-certificate "https://raw.githubusercontent.com/mouyase/ChinaListForAdGuardHome/gh-pages/ChinaList.txt" &&
-sed -i '1i 208.67.220.220:5353' ChinaList.txt &&
-sed -i '2i 208.67.222.222:5353' ChinaList.txt &&
-mv /etc/ChinaList.txt /etc/config/cl.txt &&
-/etc/init.d/AdGuardHome restart
